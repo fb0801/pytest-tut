5b2698e1206050c070728363f7c866148bd908ec
fb0801 <fbhatti0801@gmail.com>
2023-10-25 18:48:20 +0100

    fixture test 4

--- source/shapes.py
+++ source/shapes.py
@@ -25,6 +25,13 @@ class Rectangle(Shape):
         self.length = length
         self.width = width
 
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return False
+        
+        return self.width == other.width and self.length == other.length
+        
+
     def area(self):
         return self.length * self.width
     
--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -6,6 +6,10 @@ import source.shapes as shapes
 def my_rectangle():
     return shapes.Rectangle(10, 20)
 
+@pytest.fixture
+def weird_rectangle():
+    return shapes.Rectangle(5,6)
+
 def test_area(my_rectangle):
     assert my_rectangle.area() == 10 * 20
 
@@ -13,3 +17,7 @@ def test_area(my_rectangle):
 
 def test_perimeter(my_rectangle):
     assert my_rectangle.perimeter() == (10 * 2) + (20 * 2)
+
+
+def test_not_equal(my_rectangle, weird_rectangle):
+    assert my_rectangle != weird_rectangle
