3d33725174da87dde8b450af72b49eea79c784b1
fb0801 <fbhatti0801@gmail.com>
2023-10-25 18:05:41 +0100

    class test 4

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -14,5 +14,7 @@ class TestCircle:
     def test_area(self):
         assert self.circle.area() == math.pi * self.circle.radius ** 2
 
-    def test_two(self):
-        assert True
+    def test_perimeter(self):
+        result = self.circle.perimeter()
+        expected = 2 * math.pi * self.circle.radius
+        assert result == expected
