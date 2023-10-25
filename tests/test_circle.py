b38311d357274fa9e8cf2eb7170946f30680b24d
fb0801 <fbhatti0801@gmail.com>
2023-10-25 17:57:14 +0100

    class test 2

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -3,5 +3,15 @@ import source.shapes as shapes
 
 class TestCircle:
 
+    def setup_method(self, method):
+        print(f"setting up {method}")
+        self.circle = shapes.Circle(10)
+
+    def teardown_method(self, method):
+        print(f"tearing down method {method}")
+
     def test_one(self):
         assert True 
+
+    def test_two(self):
+        assert True
