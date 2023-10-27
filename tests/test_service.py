1b551fe9d7ef9539c24ccc039dd9609a068e858e
fb0801 <fbhatti0801@gmail.com>
2023-10-27 17:47:03 +0100

    mock test 1

--- tests/test_service.py
+++ tests/test_service.py
@@ -5,6 +5,9 @@ import source.service as service
 import unittest.mock as mock 
 
 
-@mock.patch("")
-def test_get_user_from_db():
-    pass
+@mock.patch("source.service.get_user_from_db")
+def test_get_user_from_db(mock_get_user_from_db):
+    mock_get_user_from_db.return_value = "Mocked Alice"
+    user_name = service.get_user_from_db(1)
+    
+    assert user_name == "Mocked Alice"
